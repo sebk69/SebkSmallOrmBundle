c9cf648ce541bf3daa62a28cdbe01da850e13c8a
Sébastien Kus <seb@ktoi.fr>
2019-01-08 18:42:18 +0100

    Bug fix in createQueryBuilder of DeleteBuilder

--- QueryBuilder/DeleteBuilder.php
+++ QueryBuilder/DeleteBuilder.php
@@ -43,7 +43,9 @@ class DeleteBuilder
     public function createQueryBuilder()
     {
         $query = $this->baseDao->createQueryBuilder($this->from->getAlias());
-        $query->setWhere(clone $this->where);
+        if($this->where !== null) {
+            $query->setWhere(clone $this->where);
+        }
         foreach ($this->getParameters() as $paramName => $paramValue) {
             $query->setParameter($paramName, $paramName);
         }
