fc38c3a8f930d1f5a615e45045d1d6a17169cfe9
Sébastien Kus <seb@small-iceberg.dev>
2024-07-19 13:41:49 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder("sebk_small_orm");
 
-        $treeBuilder->getRootNode()
+        $treeBuilder
             ->children()
                 ->scalarNode('default_connection')
                     ->defaultValue('default')
